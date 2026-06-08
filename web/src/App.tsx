b269a7a5dd7d8e36d259e68e83f1fadd4debe936
Tushar Sharma <tusharsharma86@gmail.com>
2026-06-08 19:10:53 +0530

    Update App.tsx

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -158,7 +158,7 @@ export default function App() {
                   position: 'relative',
                 }}>
                   <ScreenRenderer />
-                  <TabBar />
+                  {/* <TabBar /> */}
                 </div>
               </ThemeApplier>
             </RouterProvider>
